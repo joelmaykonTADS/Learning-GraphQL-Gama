3c9da89ff8cf6dc07825b556fcb3b18d55fbf950
joelmaykonTADS <joelmaykon94@gmail.com>
2020-06-27 18:09:22 -0300

    ativando o cors

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,21 +1,26 @@
 import express from 'express';
+import cors from 'cors'
 
 const server = express();
 
 server.get('/status', (_, res) => {
-  res.send({
-    status: 'OK'
-  })
+	res.send({
+		status: 'OK'
+	});
 });
 
-server.post('/authenticate', express.json(), (req, res) => {
-  console.log('Mail:', req.body.mail, 'Password:', req.body.password);
-  res.send();
-});
+const enableCors = cors({ origin: 'http://localhost:3000' });
 
-const ip = process.env.HOSTNAME || "127.0.0.1";
-const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+server
+	.options('/authenticate', enableCors)
+	.post('/authenticate',enableCors,express.json(), (req, res) => {
+		console.log('Mail:', req.body.email, 'Password:', req.body.password);
+		res.send();
+	});
+
+const ip = process.env.HOSTNAME || '127.0.0.1';
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 
 server.listen(port, ip, () => {
-  console.log(`Server is running at http://${ip}:${port}`);
+	console.log(`Server is running at http://${ip}:${port}`);
 });
